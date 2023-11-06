4a5343662108580f5917c939f208a0f70f71477f
Yusuf <145934201+YusufDev15@users.noreply.github.com>
2023-11-06 22:56:02 +0000

    write the questions in the javascript

---
+++ assets/js/questions.js
@@ -0,0 +1,24 @@
+//variable for questions, an array of objects
+var questions = [
+  {
+    question: "Commonly used data types DO Not Include",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "booleans",
+  },
+  {
+    question:
+      "The condition in an if / else statement is enclosed with _______.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: "curly brackets",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
+];
